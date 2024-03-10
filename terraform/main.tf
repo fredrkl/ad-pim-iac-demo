f496216f14125df3a2cb54515b78c7ddb5d64596
Fredrik Klingenberg <fredrkl@gmail.com>
2024-03-10 09:54:46 +0100

    feat: basic terraform setup
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = "northeurope"
+}
+
+terraform {
+  required_version = ">= 1.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-files"
+    storage_account_name = "terraformdemostatefiles"
+    container_name       = "ad-pim-demo-tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
