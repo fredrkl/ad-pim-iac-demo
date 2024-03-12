1464330c77abfd5ece58301358982c43ab9d2ae0
Fredrik Klingenberg <fredrkl@gmail.com>
2024-03-12 06:47:44 +0100

    feat: setting up ad provider
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.85.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.47.0"
+    }
   }
   backend "azurerm" {
     resource_group_name  = "terraform-state-files"
