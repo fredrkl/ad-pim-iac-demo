8ceb220bf098d1e0629ea9f96c4903e34993d702
Fredrik Klingenberg <fredrkl@gmail.com>
2024-03-19 11:48:05 +0100

    feat: powershell list policy definitions
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ ps/update-role-management-policy.ps1
@@ -0,0 +1,52 @@
+function listPolicyAssignments {
+  param (
+    [Parameter(Mandatory = $True)]
+    [string]$Scope,
+    [Parameter(Mandatory = $True)]
+    [string]$Role
+  )
+  
+  $ErrorActionPreference = "Stop"
+  
+  Write-Host "Scope is [$Scope]"
+  $PolicyId = Get-AzRoleManagementPolicyAssignment -Scope $Scope | Where-Object RoleDefinitionDisplayName -EQ $Role | ForEach-Object PolicyId
+  Write-Host "PolicyId is [$PolicyId] for Role [$Role]"
+
+  #$Policy = Get-AzRoleManagementPolicy -Scope $Scope | Where-Object Id -eq $PolicyId
+  
+  #
+  #$expirationRule = @{
+  #  isExpirationRequired     = "false";
+  #  maximumDuration          = "P365D";
+  #  id                       = "Expiration_Admin_Eligibility";
+  #  ruleType                 = "RoleManagementPolicyExpirationRule";
+  #  targetCaller             = "Admin";
+  #  targetOperation          = @('All');
+  #  targetLevel              = "Eligibility";
+  #  targetObject             = $null;
+  #  targetInheritableSetting = $null;
+  #  targetEnforcedSetting    = $null;
+  #}
+  #
+  #@pimRule = @{
+  #  ruleType = "RoleManagementPolicyPimRule";
+  #  id = "PIM_Admin_Eligibility";
+  #  targetCaller = "Admin";
+  #  targetOperation = @('All');
+  #  targetLevel = "Eligibility";
+  #  targetObject = $null;
+  #  targetInheritableSetting = $null;
+  #  targetEnforcedSetting = $null;
+  #  targetPimSetting = @{
+  #    isPimRequired = "true";
+  #    pimType = "PIM";
+  #    pimRole = "Owner";
+  #    pimDuration = "P7D";
+  #  }
+  #}
+  #
+  #$rules = @($expirationRule)
+  #Update-AzRoleManagementPolicy -Scope $scope -Name $Policy.Name -Rule $rules
+}
+
+listPolicyAssignments -Scope "/subscriptions/57cd39e7-07f1-4555-adea-802d4fc5a5e1" -Role "Owner"
