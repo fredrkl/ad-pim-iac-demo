7664700c64774513240d7972939c23343897f22f
Fredrik Klingenberg <fredrkl@gmail.com>
2024-04-02 16:08:35 +0200

    feat(tf): no expiration
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,20 +12,25 @@ data "azurerm_role_definition" "main" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_pim_eligible_role_assignment" "main" {
-  scope              = data.azurerm_subscription.main.id
-  role_definition_id = "${data.azurerm_subscription.main.id}${data.azurerm_role_definition.main.id}"
-  principal_id       = data.azurerm_client_config.current.object_id
+resource "random_uuid" "main" {}
 
-  schedule {
-    expiration {
-      duration_hours = 5
+resource "azapi_resource_action" "role_eligibility_schedule_request" {
+  type        = "Microsoft.Authorization/roleEligibilityScheduleRequests@2022-04-01-preview"
+  resource_id = "/subscriptions/${data.azurerm_subscription.main.subscription_id}/providers/Microsoft.Authorization/roleEligibilityScheduleRequests/${random_uuid.main.id}"
+  method      = "PUT"
+  body = jsonencode({
+    properties = {
+      principalId      = data.azurerm_client_config.current.object_id
+      requestType      = "AdminAssign"
+      roleDefinitionId = "${data.azurerm_subscription.main.id}${data.azurerm_role_definition.main.id}"
+      scheduleInfo = {
+        expiration = {
+          type = "NoExpiration"
+        }
+      }
     }
-  }
+  })
 }
-
-#resource "azapi_resource_action" "role_eligable_request" {
-#}
 
 terraform {
   required_version = ">= 1.7"
