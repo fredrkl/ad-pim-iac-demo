7ee3e255e3b885bba01a6bf5529650c7e300b37a
Fredrik Klingenberg <fredrkl@gmail.com>
2024-04-09 07:23:26 +0200

    :recycle: Make it callable from outside powershell
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- ps/update-role-management-policy.ps1
+++ ps/update-role-management-policy.ps1
@@ -1,40 +1,11 @@
-function listPolicyAssignments {
-  param (
-    [Parameter(Mandatory = $True)]
-    [string]$Scope,
-    [Parameter(Mandatory = $True)]
-    [string]$Role,
-    [Parameter(Mandatory = $False)]
-    [string]$PimGroup
-  )
-  
-  $ErrorActionPreference = "Stop"
-
-  Write-Host "Scope is [$Scope]"
-  Write-Host "Role is [$Role]"
-
-  # Get the policy assignment
-  $PolicyId = Get-AzRoleManagementPolicyAssignment -Scope $Scope | Where-Object RoleDefinitionDisplayName -EQ $Role | ForEach-Object PolicyId
-  Write-Host "PolicyId is [$PolicyId] for Role [$Role]"
-
-  # Get the policy itself
-  $Policy = Get-AzRoleManagementPolicy -Scope $Scope | Where-Object Id -eq $PolicyId
-
-  $expirationRule = createExpirationRule
-  $rules = [Microsoft.Azure.PowerShell.Cmdlets.Resources.Authorization.Models.Api20201001Preview.IRoleManagementPolicyRule[]]@($expirationRule)
-
-  # Check if the optional parameter was provided
-  if ($PSBoundParameters.ContainsKey('PimGroup')) {
-    # Need to have the full namespace for the `rule` and `ruleType`: https://github.com/Azure/azure-powershell/issues/18781
-    $pimRule = createPimRule -PimGroup $PimGroup
-    $hourPim = createHourRule
-
-    $rules += $pimRule
-    $rules += $hourPim
-  } 
-
-  Update-AzRoleManagementPolicy -Scope $Scope -Name $Policy.Name -Rule $rules -Debug
-}
+param (
+  [Parameter(Mandatory = $True)]
+  [string]$Scope,
+  [Parameter(Mandatory = $True)]
+  [string]$Role,
+  [Parameter(Mandatory = $False)]
+  [string]$PimGroup
+)
 
 function createHourRule {
   # Require dual approval for role assignments
@@ -116,5 +87,34 @@ function createExpirationRule {
   return $expirationRule
 }
 
-listPolicyAssignments -Scope "/subscriptions/57cd39e7-07f1-4555-adea-802d4fc5a5e1" -Role "Owner" "03807c38-aa7e-479b-87c1-7ef86265691e"
+
+$ErrorActionPreference = "Stop"
+
+Write-Host "Scope is [$Scope]"
+Write-Host "Role is [$Role]"
+
+# Get the policy assignment
+$PolicyId = Get-AzRoleManagementPolicyAssignment -Scope $Scope | Where-Object RoleDefinitionDisplayName -EQ $Role | ForEach-Object PolicyId
+Write-Host "PolicyId is [$PolicyId] for Role [$Role]"
+
+# Get the policy itself
+$Policy = Get-AzRoleManagementPolicy -Scope $Scope | Where-Object Id -eq $PolicyId
+
+$expirationRule = createExpirationRule
+$rules = [Microsoft.Azure.PowerShell.Cmdlets.Resources.Authorization.Models.Api20201001Preview.IRoleManagementPolicyRule[]]@($expirationRule)
+
+# Check if the optional parameter was provided
+if ($PSBoundParameters.ContainsKey('PimGroup')) {
+  # Need to have the full namespace for the `rule` and `ruleType`: https://github.com/Azure/azure-powershell/issues/18781
+  $pimRule = createPimRule -PimGroup $PimGroup
+  $hourPim = createHourRule
+
+  $rules += $pimRule
+  $rules += $hourPim
+} 
+
+Update-AzRoleManagementPolicy -Scope $Scope -Name $Policy.Name -Rule $rules -Debug
+
+#listPolicyAssignments -Scope "/subscriptions/57cd39e7-07f1-4555-adea-802d4fc5a5e1" -Role "Owner" "03807c38-aa7e-479b-87c1-7ef86265691e"
 #listPolicyAssignments -Scope "/subscriptions/57cd39e7-07f1-4555-adea-802d4fc5a5e1" -Role "Owner"
+#./update-role-management-policy.ps1 -Scope "/subscriptions/57cd39e7-07f1-4555-adea-802d4fc5a5e1" -Role "Owner" "03807c38-aa7e-479b-87c1-7ef86265691e
